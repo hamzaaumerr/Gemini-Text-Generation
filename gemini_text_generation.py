2e9460d405bc8180ca0c858c20ae02f2203c0e45
Hamza Umer <hamzakhann0666@gmail.com>
2024-08-16 12:31:44 +0500

    Initial Commit

---
+++ gemini_text_generation.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api = os.getenv('GEMINI_API')
+genai.configure(api_key=gemini_api)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+def modelResponce(user_input):
+  response = model.generate_content(user_input)
+  return(response.text)
+st.title("Google Gemini Text Generation")
+user_input = st.text_input("Enter your prompt")
+
+if st.button("Generate"):
+  if user_input:
+    output = modelResponce(user_input)
+    st.write(f"Generated Text: {output}")
+  else:
+    st.warning("Please enter a prompt.")
