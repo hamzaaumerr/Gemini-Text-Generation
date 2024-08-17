c5e9cda7eb7c0752e9312ce89d18f8562930aadb
Hamza Umer <hamzakhann0666@gmail.com>
2024-08-17 10:23:51 +0500

    intial commit

--- gemini_text_generation.py
+++ gemini_text_generation.py
@@ -1,19 +1,34 @@
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key=st.secrets["gemini_api"])
+if 'gemini_api_key' not in st.session_state:
+  st.session_state.gemini_api_key = ""
 
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-def modelResponce(user_input):
-  response = model.generate_content(user_input)
-  return(response.text)
+st.set_page_config(page_title="Text Generation", layout="centered")
 st.title("Google Gemini Text Generation")
+
+gemini_api_key = st.text_input("Enter your Google Gemini API Key", type="password")
+
+if st.session_state.gemini_api_key == "":
+  st.session_state.gemini_api_key = st.text_input("Enter your Google Gemini API Key", type="password")
+  if st.session_state.gemini_api_key:
+    st.success("API Key saved successfully!")
+else:
+    st.text("API Key is stored in session.")
+
 user_input = st.text_input("Enter your prompt")
 
+def model_responce(user_input):
+  genai.configure(api_key=gemini_api_key)
+  model = genai.GenerativeModel('gemini-1.5-flash')
+  response = model.generate_content(user_input)
+  return response.text
+
 if st.button("Generate"):
-  if user_input:
-    output = modelResponce(user_input)
-    st.write(output)
-  else:
+  if not st.session_state.api_key:
+    st.warning("Please enter your Google Gemini API key.")
+  elif not user_input:
     st.warning("Please enter a prompt.")
+  else:
+    output = model_responce(user_input)
+    st.write(output)
